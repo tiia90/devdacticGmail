93e1c4b2478b939e07d67af59eba272be1266782
tiia90 <2002094@student.laurea.fi>
2022-03-26 16:18:00 +0200

    Labwork4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,32 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'tabs',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'mail',
+    loadChildren: () => import('./pages/mail/mail.module').then( m => m.MailPageModule)
+  },
+  {
+    path: 'meet',
+    loadChildren: () => import('./pages/meet/meet.module').then( m => m.MeetPageModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./pages/account/account.module').then( m => m.AccountPageModule)
+  },
+  {
+    path: 'details',
+    loadChildren: () => import('./pages/details/details.module').then( m => m.DetailsPageModule)
   }
 ];
 
---
+++ src/app/directives/shared-directives.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HideHeaderDirective } from './hide-header.directive';
+
+
+
+@NgModule({
+  declarations: [
+    HideHeaderDirective
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [HideHeaderDirective]
+})
+
+export class SharedDirectivesModule { }
